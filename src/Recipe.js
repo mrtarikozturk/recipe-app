c2e5f568de01594a6e080f52f7665510e95036cf
mrtarikozturk <mr.tarik.ozturk@gmail.com>
2020-11-14 23:46:59 +0100

    Add ingredients and style

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import style from './recipe.module.css';
 
-const Recipe = (props) => {
+const Recipe = ({ data }) => {
     return (
-        <div>
-            <h1>{props.data.label}</h1>
-            <p>{props.data.calories}</p>
-            <img src={props.data.image} alt='' />
+        <div className={style.recipe}>
+            <h1>{data.label}</h1>
+            <ol>
+                {
+                    data.ingredients.map((item, index) => (
+                        <li>{item.text}</li>
+                    ))
+                }
+            </ol>
+            <p>{data.calories}</p>
+            <img className={style.image} src={data.image} alt='' />
         </div>
     );
 }
